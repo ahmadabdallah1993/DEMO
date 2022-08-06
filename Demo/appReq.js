c12acb3dc20eeee42fba55d2d2648e7d132a92d6
Ahmad Yaghi <ahmad.abdallah1993@outlook.com>
2022-08-06 21:37:58 +0300

    Dom

--- Demo/appReq.js
+++ Demo/appReq.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+const body = document.getElementsByTagName('body');
+
 const allDrinks = [];
 
 function Drinks( name, ingredients, image, cold, hot, price) {
@@ -12,13 +15,25 @@ function Drinks( name, ingredients, image, cold, hot, price) {
 
     allDrinks.push(this);
 };
-document.write(`<h1>Welcome to asac coffe house</h1><br><p>hello</p>`);
+document.write(`<h1>Welcome to asac coffe house</h1>`);
 Drinks.prototype.printMenue = function() {
 //console.log(`${this.name} : ${this.price} JD`);
 
 
+const pE1 = document.createElement('p');
+pE1.textContent = `Drink name : ${this.name} : ${this.price}`;
 
-document.write(`<p>${this.name} : ${this.price} JD</p>`)
+body[0].appendChild(pE1);
+
+
+
+const imgE1 = document.createElement('img');
+imgE1.src = this.image;
+imgE1.alt = this.name;
+body[0].appendChild(imgE1);
+
+
+// document.write(`<p>${this.name} : ${this.price} JD</p>`)
 
 
 }
@@ -50,7 +65,7 @@ console.dir(document)
 
 
 function render() {
-    document.write(`<p>${espresso.name}</p>`);
+    // document.write(`<p>${espresso.name}</p>`);
 }
 
 render();
